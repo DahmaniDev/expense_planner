9d1bf64e134ea18447c01a9ebf3638d3260fcecc
Tuxette Chix <tuxette@chix.nathalievilla.org>
2020-01-09 23:56:53 +0100

    First Git upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,17 +14,64 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 50.0, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Groceries', amount: 80.0, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(
-        child: Column(
-
+        appBar: AppBar(
+          title: Text('Flutter App'),
         ),
-      ),
-    );
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Card(
+              child: Container(
+                child: Text('CHART!'),
+                width: double.infinity,
+              ),
+              color: Colors.red,
+              elevation: 5.0,
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        tx.amount.toString()+r'$',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      children: <Widget>[
+                        Text(tx.title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                        Text(tx.date.toString(), style: TextStyle(color: Colors.grey),)
+                      ],
+                    )
+                  ],
+                ));
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
